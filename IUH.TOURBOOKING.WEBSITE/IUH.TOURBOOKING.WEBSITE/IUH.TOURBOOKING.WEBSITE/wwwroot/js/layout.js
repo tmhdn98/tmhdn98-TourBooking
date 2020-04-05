6325caa22fca18b4295374c609fafd73d739854a
TinNguyen <nguyenhoangtin3498@gmail.com>
2020-04-05 11:14:32 +0700

    no message

---
+++ IUH.TOURBOOKING.WEBSITE/IUH.TOURBOOKING.WEBSITE/IUH.TOURBOOKING.WEBSITE/wwwroot/js/layout.js
@@ -0,0 +1,8 @@
+﻿$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+
